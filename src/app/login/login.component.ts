9794c61e13753acf5fe90dcf2d0bdecc564cd273
Masayuki Sugahara <equaaqua@hotmail.com>
2022-05-06 22:41:38 +0900

    add: save last used index

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTabGroup } from '@angular/material/tabs';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements AfterViewInit {
+  private lastLoginMethodIndexKey = 'login.lastLoginMethodIndex';
+  @ViewChild('tabGroup') tabGroup?: MatTabGroup;
+
+  constructor(private storage: LocalStorageService) {}
+
+  ngAfterViewInit(): void {
+    if (!this.tabGroup) return;
+
+    const index = Number(
+      this.storage.retrieve(this.lastLoginMethodIndexKey) ?? 0
+    );
+    this.tabGroup.selectedIndex = index;
+  }
+
+  setLastLoginMethodIndex(index: number) {
+    this.storage.store(this.lastLoginMethodIndexKey, index);
+  }
+}
