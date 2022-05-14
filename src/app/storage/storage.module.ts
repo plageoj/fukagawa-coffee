2a49a4288b67c47a83651ec882cc45395330fac8
Masayuki Sugahara <equaaqua@hotmail.com>
2022-05-14 20:38:43 +0900

    add: storage list component

---
+++ src/app/storage/storage.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StorageRoutingModule } from './storage-routing.module';
+import { StorageListComponent } from './storage-list/storage-list.component';
+
+
+@NgModule({
+  declarations: [
+    StorageListComponent
+  ],
+  imports: [
+    CommonModule,
+    StorageRoutingModule
+  ]
+})
+export class StorageModule { }
