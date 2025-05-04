b619c06ad3f8cf28beccf3938acf84695281dc49
Masayuki Sugahara <equaaqua@hotmail.com>
2025-05-04 17:44:05 +0900

    refactor: reorganize imports and enhance constructor readability in OrderListComponent

--- src/app/order/order-list/order-list.component.ts
+++ src/app/order/order-list/order-list.component.ts
@@ -1,31 +1,35 @@
+import { AsyncPipe, DatePipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { where } from '@angular/fire/firestore';
+import {
+  MatListItem,
+  MatListItemLine,
+  MatNavList,
+} from '@angular/material/list';
+import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { OrderService } from 'src/app/services/order.service';
 import { Order } from 'src/models/order.model';
-import { RouterLink } from '@angular/router';
-import { NgFor, NgIf, AsyncPipe, DatePipe } from '@angular/common';
-import { MatNavList, MatListItem, MatListItemLine } from '@angular/material/list';
 
 @Component({
-    selector: 'app-order-list',
-    templateUrl: './order-list.component.html',
-    styleUrls: ['./order-list.component.scss'],
-    imports: [
-        MatNavList,
-        NgFor,
-        MatListItem,
-        RouterLink,
-        MatListItemLine,
-        NgIf,
-        AsyncPipe,
-        DatePipe,
-    ]
+  selector: 'app-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.scss'],
+  imports: [
+    MatNavList,
+    NgFor,
+    MatListItem,
+    RouterLink,
+    MatListItemLine,
+    NgIf,
+    AsyncPipe,
+    DatePipe,
+  ],
 })
 export class OrderListComponent {
   orders: Observable<Order[]>;
 
-  constructor(private os: OrderService) {
+  constructor(private readonly os: OrderService) {
     this.orders = this.os.list(where('isDone', '==', false));
   }
 }
